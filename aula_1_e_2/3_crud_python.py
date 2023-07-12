ac01a38d53b276d95f14d46c4f37521a93452f4d
marinapisa <pisamarinaa@gmail.com>
2023-07-11 22:47:55 -0300

    update

--- aula_1_e_2/3_crud_python.py
+++ aula_1_e_2/3_crud_python.py
@@ -38,24 +38,43 @@ while True:
             print(item)
 
     elif operacao == '2':
+        os.system('cls')
         # ler o id da coluna a ser deletada
+        id = input('Digite o ID do registro que deseja deletar: ')
 
         # executar o código sql
+        cursor.execute(f'''
+            DELETE FROM usuarios
+            WHERE id = id 
+         ''')
 
         # printar mensagem de sucesso para quando deletar
+        print('Registro com id {id} deletado com sucesso!')
 
         ...
     elif operacao == '3':
+        os.system('cls')
         # ler  id da coluna a modificar
-
-
+        id = input('Informe o ID do registro que deseja alterar: ')
+        # PEDIR QUAL INFORMAÇÃO DO REGISTRO DESEJA ALTERAR E FAZER UM EXECUTE PARA CADA ATRIBUTO?
         # executar o sódigo sql
+        cursor.execute(f''' 
+            UPDATE usuarios
+            SET nome = 'TESTE'
+            WHERE NOME = 'Marina';
+        ''')
 
         #  printar a mesagem de sucesso quando atualizar
+        print('Registro com id {id} alterado com sucesso!')
 
         ...
     elif operacao == '4':
+        os.system('cls')
         # ler os dados novos
+        cursor.execute(f'''
+            INSERT INTO {contatos}(nome, idade, email)
+            VALUES ('{usuario_nome}', {usuario_idade} , '{usuario_email}')
+        ''')
 
 
         # validar os dados
@@ -73,4 +92,4 @@ while True:
 # print('Comitou dados com sucesso!!')
 # # fechar a conexão com a base de dados
 # conexao.close()
-# print('conexão fechada com sucesso!!')
+# print('conexão fechada com sucesso!!')
